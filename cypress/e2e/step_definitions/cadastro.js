1c6a4fe431d5d114cd1ee9c105baf35b2f2e8315
Yuster <yusteralmeida@gmail.com>
2023-11-27 22:40:37 -0300

    adição do teste de como cadastrar um usuário com um email existente

--- cypress/e2e/step_definitions/cadastro.js
+++ cypress/e2e/step_definitions/cadastro.js
@@ -9,5 +9,14 @@ When ('preencher o campo "Email"',() => {
  })
 
  When ('clicar no botão "Registar"',() => {
-    cy.get('p input[value=Register]').click({force: true})
+    cy.get('p input[value=Register]').dblclick({force: true})
+ })
+
+ When ('preencher o campo "Email" com um e-mail já existente no sistema',() => {
+    cy.get('#reg_email').click().type('teste@gmail.com')
+ })
+
+ When ('o sistema deve mostrar uma mensagem informando "Error: An account is already registered with your email address. Please login."',() => {
+    cy.get('li strong').should('have.text', 'Error:')
+    cy.get('ul[class="woocommerce-error"] li').should('have.text', 'An account is already registered with your email address. Please login.')
  })
